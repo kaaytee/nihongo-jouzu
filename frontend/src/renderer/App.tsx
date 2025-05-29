70eaa560f7ed1264118fc87977fb2b0ad61e9f77
kaaytee <119795949+kaaytee@users.noreply.github.com>
2025-05-29 21:28:01 +1000

    moved <HashRouter> into index.tsx

--- frontend/src/renderer/App.tsx
+++ frontend/src/renderer/App.tsx
@@ -1,43 +1,41 @@
-import { MemoryRouter as Router, Routes, Route, BrowserRouter, HashRouter, useNavigate } from 'react-router-dom';
+import { MemoryRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import icon from '../../assets/icon.svg';
 import './App.css';
 import { Scanner } from './pages/Scanner';
-import { Button } from '@mui/material';
 import { Analysis } from './pages/Analysis';
 import { Dictionary } from './pages/Dictionary';
+import { useEffect } from 'react';
 
-function Hello() {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  );
-}
 
 export default function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate('/scan');
+    }
+  }, [location.pathname, navigate]);
+
   return (
     <>
-      <div className='app'>
+        <div className='app'>
+          <div className='app-header'>
+            <h1>{location.pathname}</h1>
+          </div>
+          <div className='app-content'>
+              <Routes>
+                <Route path="/" element={<Scanner/>} />
+                {/* <Route path="/login" element={<LoginPage />} /> */}
+                {/* <Route path="/register" element={<RegisterPage />} /> */}
+                <Route path="/scan" element={<Scanner />} />
+                <Route path="/analysis" element={<Analysis />} />
+                <Route path="/dictionary/:phrase" element={<Dictionary />} />
 
-        <div className='app-header'>
-          <h1>Hello</h1>
+              </Routes>
+          </div>
         </div>
-        <div className='app-content'>
-          <HashRouter>
-            <Routes>
-              <Route path="/" element={<Scanner/>} />
-              {/* <Route path="/login" element={<LoginPage />} /> */}
-              {/* <Route path="/register" element={<RegisterPage />} /> */}
-              <Route path="/scan" element={<Scanner />} />
-              <Route path="/analysis" element={<Analysis />} />
-              <Route path="/dictionary/:phrase" element={<Dictionary />} />
-
-            </Routes>
-          </HashRouter>
-        </div>
-      </div>
 
     </>
   );
--- frontend/src/renderer/index.tsx
+++ frontend/src/renderer/index.tsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
-root.render(<App />);
+root.render(<HashRouter><App /></HashRouter>);
 
 // calling IPC exposed from preload script
 window.electron?.ipcRenderer.once('ipc-example', (arg) => {
